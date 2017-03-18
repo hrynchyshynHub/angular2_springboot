77135eaef5d626db5b4f14ce3d1e8b1ea0e3f5f4
ivanHrynchyshyn <vaniahrynchsyhyn@gmail.com>
2017-03-18 21:41:45 +0200

    'init'

---
+++ src/main/java/com/example/SptingBootAngular2Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptingBootAngular2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SptingBootAngular2Application.class, args);
+	}
+}
